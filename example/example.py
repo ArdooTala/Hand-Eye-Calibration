805608f85ab2703e5caf72aacd59c34cb9ba7021
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-12-02 19:34:00 +0100

    WIP

--- example/example.py
+++ example/example.py
@@ -1,11 +1,22 @@
+import cv2
+from pathlib import Path
+
 from hand_eye_calibration.image_processing import charuco_detector
 from hand_eye_calibration.robot_model import robot_model
 from hand_eye_calibration.hand_eye_calibrator import HandEyeCalibrator
 
 
 # Detect CharucoBoards
-det = charuco_detector.CharucoDetector("data/charuco_board.yaml", verbose=False)
-det.load_images("data/images", extension="png")
+det = charuco_detector.CharucoDetector("data/charuco_board.yaml", verbose=True)
+
+# Alt 1
+det.load_images_from_files("data/images", extension="png")
+# Alt 2
+# images = map(cv2.imread, Path("data/images").glob("*.png"))
+# det.load_images(enumerate(images))
+
+# print(len(det.images))
+# print(len(det.images))
 det.auto_detect_camera_parameters()
 det.camera.write_camera_calibration()   # Write the Camera Calibration Result
 
