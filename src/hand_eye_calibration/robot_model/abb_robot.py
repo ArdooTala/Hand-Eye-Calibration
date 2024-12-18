0c831d14e7d8a7c75cfd2acdb36cb9ffce8505f7
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-12-18 13:21:09 +0100

    Add ABB Robot Model

---
+++ src/hand_eye_calibration/robot_model/abb_robot.py
@@ -0,0 +1,16 @@
+from hand_eye_calibration.robot_model import base_robot_model
+from scipy.spatial.transform import Rotation
+
+class AbbRobot(base_robot_model.RobotModel):
+    def __init__(self):
+        super().__init__("ABB")
+        self._dist_scale = 0.001
+
+    def rotation_to_matrix(self, angles):
+        angles = super().rotation_to_matrix(angles)
+        assert len(angles) == 4
+        return Rotation.from_quat(angles).as_matrix()
+
+    def matrix_to_rotation(self, rot_matrix):
+        rot_matrix = super().matrix_to_rotation(rot_matrix)
+        return Rotation.from_matrix(rot_matrix).as_quat()
